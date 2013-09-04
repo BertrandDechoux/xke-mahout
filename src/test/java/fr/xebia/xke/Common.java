799d37ef854d6483a38d67519ce17302daf7f617
Bertrand Dechoux <dechouxb@gmail.com>
2013-09-04 17:28:21 +0200

    add missing class

---
+++ src/test/java/fr/xebia/xke/Common.java
@@ -0,0 +1,10 @@
+package fr.xebia.xke;
+
+public class Common {
+	
+	/**
+	 * Grouplens's MovieLens Data Sets downloaded at http://www.grouplens.org/node/12.
+	 */
+	public static final String DATASET_FILENAME = "src/test/resources/ml-100k/ua.base";
+
+}
